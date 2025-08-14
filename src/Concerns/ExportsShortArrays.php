514c3febe799ae4f2468f8e4eff1ba94a3643b5c
Artem Ryazanov <artryazanov@gmail.com>
2025-08-14 23:27:28 +0700

    Normalize `varExportShort` output to conform with PSR-12 indentation standards

--- src/Concerns/ExportsShortArrays.php
+++ src/Concerns/ExportsShortArrays.php
@@ -6,12 +6,25 @@ trait ExportsShortArrays
 {
     /**
      * Export arrays using short syntax [] instead of array().
+     * Also normalize indentation to PSR-12 (4 spaces).
      */
     protected function varExportShort(mixed $expression): string
     {
         $export = var_export($expression, true);
         $export = preg_replace(['/(^|\b)array\s*\(/', '/\)(?=\s*(,|\)|$))/'], ['[', ']'], $export);
 
-        return is_string($export) ? $export : var_export($expression, true);
+        if (! is_string($export)) {
+            $export = var_export($expression, true);
+            $export = preg_replace(['/(^|\b)array\s*\(/', '/\)(?=\s*(,|\)|$))/'], ['[', ']'], $export);
+        }
+
+        // Normalize indentation: var_export uses 2 spaces per level; convert to 4 spaces per level (PSR-12).
+        $export = preg_replace_callback('/^ +/m', static function (array $m) {
+            $spaces = strlen($m[0]);
+            $levels = intdiv($spaces, 2);
+            return str_repeat('    ', $levels);
+        }, $export);
+
+        return $export;
     }
 }
